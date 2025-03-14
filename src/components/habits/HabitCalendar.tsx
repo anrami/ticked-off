d0f9bad4fcf915beb324f1419d3b3fce182d54d0
Andrew Ramirez <work@andrewramirez.io>
2025-03-14 16:54:18 -0700

    add checkbox animations to calendar

--- src/components/habits/HabitCalendar.tsx
+++ src/components/habits/HabitCalendar.tsx
@@ -52,7 +52,7 @@ export default function HabitCalendar({ habits, onToggleCompletion }: HabitCalen
     const days = getAllDaysInMonth();
 
     return (
-        <section className="ant-layout layout" style={{ paddingTop: '2rem' }}>
+        <section className="ant-layout layout">
             <div className="ant-row">
                 <div className="ant-col habit-table-container mobile ant-col-xs-24 ant-col-xs-offset-0 ant-col-sm-24 ant-col-sm-offset-0 ant-col-md-22 ant-col-md-offset-1">
                     <h4 className="month-navigator">
@@ -118,7 +118,20 @@ export default function HabitCalendar({ habits, onToggleCompletion }: HabitCalen
                                                             >
                                                                 <div className="cell-content">
                                                                     {isCompleted && (
-                                                                        <div className="completed" />
+                                                                        <div className="completed">
+                                                                            <svg
+                                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                                viewBox="0 0 24 24"
+                                                                                fill="white"
+                                                                                className="checkmark"
+                                                                            >
+                                                                                <path
+                                                                                    fillRule="evenodd"
+                                                                                    d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+                                                                                    clipRule="evenodd"
+                                                                                />
+                                                                            </svg>
+                                                                        </div>
                                                                     )}
                                                                 </div>
                                                             </td>
